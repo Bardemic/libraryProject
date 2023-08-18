26b4753a78f4e081ca973c0c11edcc0a07796832
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-08-18 15:12:09 -0500

    Add Grid, Styles, Books, Delete Button

--- script.js
+++ script.js
@@ -21,9 +21,28 @@ function updateBooksList() {
     booksContainer.innerHTML = "";
     for(var i = 0; i < myLibrary.length; i++) {
         const bookDiv = document.createElement("div");
+        bookDiv.setAttribute("id", i);
         const title = document.createElement("h3");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const delButton = document.createElement("button");
+        author.innerHTML = `Author: ${myLibrary[i].author}`;
         title.innerHTML = myLibrary[i].title;
+        pages.innerHTML =  `Pages: ${myLibrary[i].pages}`;
+        delButton.addEventListener('click', (e) => {
+            console.log(myLibrary.splice(e.target.parentElement.id, e.target.parentElement.id + 1));
+            updateBooksList();
+        })
         bookDiv.append(title);
+        bookDiv.append(author);
+        bookDiv.append(pages);
+        bookDiv.append(delButton);
         booksContainer.append(bookDiv);
     }
 }
+
+const book1 = new book("One", "1", "One");
+const book2 = new book("Two", "2", "Two");
+const book3 = new book("Three", "3", "Three");
+myLibrary.push(book1, book2, book3);
+updateBooksList();
